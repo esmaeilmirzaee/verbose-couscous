485b103c349faa2b51b47df69a058c1e170d00be
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-05-14 15:50:28 +0430

    Setup the application

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import './App.css';
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
+            <header className='App-header'>{/* Header component */}</header>
+            <section>{/* main and middle part of the app */}</section>
+
+            <footer>{/* footer */}</footer>
         </div>
     );
 }
